2c2022b53becfd63203e3b0da365dd60cdb390d6
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2023-11-06 15:36:54 -0400

    add Temp Data tiles to the Settings pages

--- Source/SettingsPage.cpp
+++ Source/SettingsPage.cpp
@@ -7,18 +7,17 @@
 
   ==============================================================================
 */
-
 #include <JuceHeader.h>
 #include "SettingsPage.h"
 
-SettingsPage::SettingsPage(){
+SettingsPage::SettingsPage()
+{
    
 
     // Create a viewport and set its size
     addAndMakeVisible(viewport);
     viewport.setViewedComponent(&m_ViewedContent, false);
     viewport.setScrollBarsShown(false, false);
-    
 }
 
 
@@ -29,24 +28,42 @@ void SettingsPage::resized()
     viewport.setBounds(getLocalBounds());
     
     // Set the content size
-    m_ViewedContent.setBounds(0, 0, getWidth(), 800);
-
+    m_ViewedContent.setBounds(0, 0, getWidth(), m_ViewedContent.getPageHeight());
+    
 }
 //========================================================================================
 
 SettingsPage::SettingsPageViewedContent::SettingsPageViewedContent() { // constructor of the content view
    
-
+    // temp vector of data for testing
+    Data data_temp;
+    std::vector<Data*> dataList_temp;
+    dataList_temp.push_back(&data_temp);
+    
+    int n = 15; //create "n" temp tile
+    for(int i = 0; i < n; i++){
+        createNewDataTile(dataList_temp);
+    }
+    
+    // add The tile to the viewedContent
+    for (PageTile* dataTile : m_SettingsPageDataTiles ) {
+        addAndMakeVisible(dataTile);
+    }
+    
+    
 
 }
 
 SettingsPage::SettingsPageViewedContent::~SettingsPageViewedContent() { // destructor of the content view
     
-    
+    // manual garbage collection for the ``` m_SettingsDataTiles```
+    for (PageTile* x : m_SettingsPageDataTiles) {
+        delete x;
+    }
 }
 
 void SettingsPage::SettingsPageViewedContent::paint(juce::Graphics & g){ // paint method of content view
-  
+    
     if(juce::Desktop::getInstance().isDarkModeActive() == true){
         g.fillAll(ColourPalette::getDarkModeBgColour());
     }
@@ -63,13 +80,36 @@ void SettingsPage::SettingsPageViewedContent::paint(juce::Graphics & g){ // pain
     else {
         g.setColour(ColourPalette::getLightModeMainTextColour());
     }
-    g.setFont(14.0f);
-    g.drawText("Settings Page is Under Maintenance", getLocalBounds(), juce::Justification::centred, true);
+    
     
 }
 
 void SettingsPage::SettingsPageViewedContent::resized() { // resized method of the content view
+    // set the position of the tiles
+    auto area = getLocalBounds();
+    int spacerSize = 1;
     
-    
-    
+    for (int i = 0 ; i <  m_SettingsPageDataTiles.size(); i++) {
+        m_SettingsPageDataTiles[i]->setBounds(area.removeFromTop(80));
+        area.removeFromTop(spacerSize);
+                
+    }
+    m_PageHeight = (int)m_SettingsPageDataTiles.size() * (80 + spacerSize); //assigning the value of the page's height
+   
+
+
 }
+
+void SettingsPage::SettingsPageViewedContent::createNewDataTile(std::vector<Data*> data) {
+    
+    PageTile* newTile = new PageTile();  // Create a new Tile Dynamically
+    
+    newTile->passData(data);     // add the vector of data to the new Tile
+    
+    
+    m_SettingsPageDataTiles.push_back(newTile);//add the tile to the homePageTile vector
+
+    // resize the viewport
+   // resized();
+}
+
--- Source/SettingsPage.h
+++ Source/SettingsPage.h
@@ -12,21 +12,18 @@
 
 #include <JuceHeader.h>
 #include "ColourPalette.h"
+#include "PageTile.h"
+#include "Data.h"
+
 //==============================================================================
-/*
-*/
-
-
 class SettingsPage : public juce::Component
 {
 public:
     SettingsPage();
-    ~SettingsPage(){}
+    ~SettingsPage() {}
     void resized() override;
 
 private:
-    
-    //=========================================================
     class SettingsPageViewedContent : public juce::Component {
     public:
         SettingsPageViewedContent();
@@ -34,18 +31,23 @@ private:
 
         void paint(juce::Graphics&) override;
         void resized() override;
+
+        void createNewDataTile(std::vector<Data*> data); // Similar to the createNewDataTile in HomePage
+
+        int getPageHeight() { return m_PageHeight; }
         
     private:
-        
+        std::vector<PageTile*> m_SettingsPageDataTiles; // Similar to m_SettingsPageTiles in HomePage
+
+        int m_PageHeight;
+
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SettingsPageViewedContent)
     };
 
-    
     SettingsPageViewedContent m_ViewedContent;
     juce::Viewport viewport;
-    
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SettingsPage)
 };
-
 
 
