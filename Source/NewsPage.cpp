91f4fd4a6ef7ecb919dfd5e01e61126e5b390ce1
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2023-11-06 15:56:16 -0400

    add temp news tiles to the news page

--- Source/NewsPage.cpp
+++ Source/NewsPage.cpp
@@ -19,7 +19,6 @@ NewsPage::NewsPage()
     addAndMakeVisible(viewport);
     viewport.setViewedComponent(&m_ViewedContent, false);
     viewport.setScrollBarsShown(false, false);
-    
 }
 
 
@@ -30,23 +29,42 @@ void NewsPage::resized()
     viewport.setBounds(getLocalBounds());
     
     // Set the content size
-    m_ViewedContent.setBounds(0, 0, getWidth(), 800);
-
+    m_ViewedContent.setBounds(0, 0, getWidth(), m_ViewedContent.getPageHeight());
+    
 }
 //========================================================================================
 
 NewsPage::NewsPageViewedContent::NewsPageViewedContent() { // constructor of the content view
    
-
+    // temp vector of data for testing
+    Data data_temp;
+    std::vector<Data*> dataList_temp;
+    dataList_temp.push_back(&data_temp);
+    
+    int n = 20; //create "n" temp tile
+    for(int i = 0; i < n; i++){
+        createNewDataTile(dataList_temp);
+    }
+    
+    // add The tile to the viewedContent
+    for (PageTile* dataTile : m_NewsPageTiles ) {
+        addAndMakeVisible(dataTile);
+    }
+    
+    
 
 }
 
 NewsPage::NewsPageViewedContent::~NewsPageViewedContent() { // destructor of the content view
     
-    
+    // manual garbage collection for the ``` m_HomeDataTiles```
+    for (PageTile* x : m_NewsPageTiles) {
+        delete x;
+    }
 }
 
 void NewsPage::NewsPageViewedContent::paint(juce::Graphics & g){ // paint method of content view
+    
     if(juce::Desktop::getInstance().isDarkModeActive() == true){
         g.fillAll(ColourPalette::getDarkModeBgColour());
     }
@@ -63,14 +81,36 @@ void NewsPage::NewsPageViewedContent::paint(juce::Graphics & g){ // paint method
     else {
         g.setColour(ColourPalette::getLightModeMainTextColour());
     }
-    g.setFont(14.0f);
-    g.drawText("News Page is Under Maintenance", getLocalBounds(), juce::Justification::centred, true);
+    
     
 }
 
 void NewsPage::NewsPageViewedContent::resized() { // resized method of the content view
+    // set the position of the tiles
+    auto area = getLocalBounds();
+    int spacerSize = 1;
+    
+    for (int i = 0 ; i <  m_NewsPageTiles.size(); i++) {
+        m_NewsPageTiles[i]->setBounds(area.removeFromTop(300));
+        area.removeFromTop(spacerSize);
+                
+    }
+    m_PageHeight = (int)m_NewsPageTiles.size() * (300 + spacerSize); //assigning the value of the page's height
+   
+
+
+}
+
+void NewsPage::NewsPageViewedContent::createNewDataTile(std::vector<Data*> data) {
+    
+    PageTile* newTile = new PageTile();  // Create a new Tile Dynamically
+    
+    newTile->passData(data);     // add the vector of data to the new Tile
     
     
-    
+    m_NewsPageTiles.push_back(newTile);//add the tile to the homePageTile vector
+
+    // resize the viewport
+   // resized();
 }
 
--- Source/NewsPage.h
+++ Source/NewsPage.h
@@ -12,16 +12,20 @@
 
 #include <JuceHeader.h>
 #include "ColourPalette.h"
+#include "PageTile.h"
+#include "Data.h"
+
 //==============================================================================
 /*
 */
+
+
 
 class NewsPage : public juce::Component
 {
 public:
     NewsPage();
     ~NewsPage(){}
-
     void resized() override;
 
 private:
@@ -34,7 +38,19 @@ private:
 
         void paint(juce::Graphics&) override;
         void resized() override;
+        
+        // creates a new data tile and pushes it to homeDataTiles
+        void createNewDataTile(std::vector<Data*> data); //TODO: Not sure if this should me a limited or unlimtied vector
+        
+        int getPageHeight() {return m_PageHeight;}
+        
     private:
+        
+        //vector of tiles based on data from from the data model
+        std::vector<PageTile*> m_NewsPageTiles;  //TODO: figure out a way to use smart pointers for auto garbage collection
+        
+        //stores the value of the bottom of the page
+        int m_PageHeight;
         
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(NewsPageViewedContent)
     };
@@ -42,7 +58,7 @@ private:
     
     NewsPageViewedContent m_ViewedContent;
     juce::Viewport viewport;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(NewsPage)
 };
-
 
