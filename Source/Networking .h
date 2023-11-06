3507ec65480b137de700b6fdccb63d1f052a4f60
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2023-11-06 13:16:40 -0400

    Add Networking class to the Project
    Networking Class is responsible for making requests to the MLAB Server and the response is stored in Data Objects.

---
+++ Source/Networking .h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    Networking .h
+    Created: 6 Nov 2023 1:09:19pm
+    Author:  Shawn Joseph
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "Data.h"
+
+/**
+ This class is resposible for making HTTP requests to the MLAB API and packaging the response into a Data object
+ */
+
+class Networking {
+    
+    
+};
